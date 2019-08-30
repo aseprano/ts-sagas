5e006474c468e1d8341af0f00853b365128c4eb8
Antonio Seprano <antonio.seprano@docebo.com>
2019-08-30 08:34:12 +0200

    Using the SagaBuilder in the AbstractSaga

--- src/impl/AbstractSaga.ts
+++ src/impl/AbstractSaga.ts
@@ -1,12 +1,24 @@
 import { Saga } from "../Saga";
 import { Step } from "../Step";
+import { SagaBuilder } from "./SagaBuilder";
 
 export abstract class AbstractSaga<T> implements Saga<T> {
+    private steps?: Step<T>[] = undefined;
 
     constructor(private state: T) {}
 
-    abstract getSteps(): Step<T>[];
+    getSteps(): Step<T>[] {
+        if (this.steps === undefined) {
+            const builder = new SagaBuilder<T>();
+            this.buildSaga(builder);
+            this.steps = builder.getSteps();
+        }
+
+        return this.steps;
+    }
     
+    abstract buildSaga(builder: SagaBuilder<T>): void;
+
     getState(): T {
         return this.state;
     }

bd27b01328c4198816a68650d033cf11facaa911
Antonio Seprano <antonio.seprano@docebo.com>
2019-09-02 22:59:29 +0200

    Added the types declaration

---
+++ types/types.d.ts
@@ -0,0 +1,17 @@
+import { Action } from "../src/Action";
+import { Step } from "../src/Step";
+import { Saga } from "../src/Saga";
+import { SagaRunner } from "../src/SagaRunner";
+import { AbstractSaga } from "../src/impl/AbstractSaga";
+import { AbstractStep } from "../src/impl/AbstractStep";
+import { SagaBuilder } from "../src/impl/SagaBuilder";
+
+export {
+    Action,
+    Step,
+    Saga,
+    SagaRunner,
+    AbstractSaga,
+    AbstractStep,
+    SagaBuilder,
+};

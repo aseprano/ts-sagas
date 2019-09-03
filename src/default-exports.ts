5a38c255a12d6c6d2d86d7774e3840414a0dd1c4
Antonio Seprano <antonio.seprano@docebo.com>
2019-09-03 22:26:15 +0200

    Made the package working outside here

---
+++ src/default-exports.ts
@@ -0,0 +1,7 @@
+export * from './Action';
+export * from './Step';
+export * from './Saga';
+export * from './SagaRunner';
+export * from './impl/AbstractSaga';
+export * from './impl/AbstractStep';
+export * from './impl/SagaBuilder';
